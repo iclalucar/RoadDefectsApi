875d2fc7c1beb9686303db828fd0eac5aea1c9b6
iclalucar <iclalucar42>
2024-11-21 00:06:53 +0300

    Bugfixes

--- obj/Debug/net8.0/RoadDefectsDetection.Server.AssemblyInfo.cs
+++ obj/Debug/net8.0/RoadDefectsDetection.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoadDefectsDetection.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1104db429175d7f1742a9ec43c4e7f3001b8de4")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoadDefectsDetection.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoadDefectsDetection.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
